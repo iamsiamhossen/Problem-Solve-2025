d5d811a8c942bb618ba7bd6bb8d16f50990e266e
iamsiamhossen <siamhossencse29@gmail.com>
2025-01-02 00:03:10 +0600

    NO

---
+++ A_Preparing_for_the_Olympiad.cpp
@@ -0,0 +1,62 @@
+/**
+ * Author: iamsiamhossen
+ * Created: 01-01-2025 23:05:27
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+// fastread
+#define fastread() (ios_base::sync_with_stdio(0), cin.tie(0))
+// Shortcut
+#define endl "\n"
+#define int long long
+#define float double
+#define all(X) (X).begin(), (X).end()
+#define Reverse(X) reverse(All(X))
+#define Unique(X) (X).erase(unique((X).begin(), (X).end()), (X).end())
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define Yes cout << "Yes\n"
+#define No cout << "No\n"
+// MOD
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+#define INF 1001001001
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    vector<int> b(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cin >> b[i];
+    }
+
+    int total = a[n - 1];
+    for (int i = 0; i < n - 1; i++)
+    {
+        int tmp = a[i] - b[i + 1];
+        if (tmp >= 0)
+        {
+            total += tmp;
+        }
+    }
+    cout << total << endl;
+}
+int32_t main()
+{
+    fastread();
+    int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++)
+    {
+        // cout << "Case " << t << ": ";
+        solve();
+    }
+    return 0;
+}
